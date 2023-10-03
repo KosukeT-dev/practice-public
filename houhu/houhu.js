6834c17d82055c04f8fa0818b5a23df8285046ef
KT <98013847+KosukeT-dev@users.noreply.github.com>
2023-10-03 21:08:21 +0900

    Add files via upload

---
+++ houhu/houhu.js
@@ -0,0 +1,5 @@
+function houhu(){
+  const inputArea = document.getElementById('input-area');
+  const outputArea = document.getElementById('output-area');
+  outputArea.innerHTML = inputArea.value;
+}
